cca913d5e52823b360838b509a858e036755a7f9
cloesd22 <cloesd22@gmail.com>
2017-09-22 19:50:47 +1000

    changed styling and neatened code

--- app.js
+++ app.js
@@ -23,27 +23,13 @@ app.set('view engine','hbs');
 //responses
 app.get('/',(req,res)=>{
 	res.render('thebutton.hbs',currentcount);
+	
 })
 
 app.post('/push',(req,res)=>{
 
-	axios.get('https://btnproject-eef7a.firebaseio.com/counter.json').then((resp)=>{
-
-		console.log(resp.data.value);
-		var latestValue = resp.data.value;
-		latestValue = latestValue +1;
-
-		axios.put('https://btnproject-eef7a.firebaseio.com/counter.json',{value:latestValue}).then((resp)=>{
-
-		}).then(()=>{
-			console.log("aymen"+latestValue)
-			res.send({value:latestValue});
-		})
-
-
-		
-	});
-
+	getandIncrement(res,1);
+	
 })
 
 app.get('/hello',(req,res)=>{
@@ -52,6 +38,25 @@ app.get('/hello',(req,res)=>{
 	})
 })
 
+
+var getandIncrement = (res,data)=>{
+	//gets increments by argument and returns value. 
+	//first argument is the response object, that has method .send to send to response.
+
+	axios.get('https://btnproject-eef7a.firebaseio.com/counter.json').then((resp)=>{
+
+		console.log(resp.data.value);
+		var latestValue = resp.data.value;
+		latestValue = latestValue +data;
+
+		axios.put('https://btnproject-eef7a.firebaseio.com/counter.json',{value:latestValue}).then((resp)=>{
+
+		}).then(()=>{
+			console.log("aymen"+latestValue)
+			res.send({value:latestValue});
+		})
+	});
+}
 
 /*app.get('/*',(req,res)=>{
 	res.send('Wer u goin boiss?');
