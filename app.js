aa4c5ed621dc67120d4312a44fb77bfd5b97915b
cloesd22 <cloesd22@gmail.com>
2017-09-26 09:49:54 +1000

    Added error handling and unique IP filtering;

--- app.js
+++ app.js
@@ -14,13 +14,19 @@ const express = require('express');
 const axios = require('axios');
 const hbs = require('hbs');
 const dd = require('ddos');
-var gl = require('geoip-lite');
+const gl = require('geoip-lite');
+const uid = require('uniqid');
+
+
+
 
 
 var ddos = new dd({ burst: 1, limit: 1 })
 var app = express();
+
 app.use('/pushFor', ddos.express);
 app.use('/pushAgainst', ddos.express);
+
 
 const port = process.env.PORT || 3001;
 //setup
@@ -50,20 +56,82 @@ app.get('/', (req, res) => {
 			for: currentFor,
 			against: currentAgainst
 		});
+	}).catch((err) => {
+		console.log("Error getting base file data -" + err);
 	});
 
 
 })
 
+app.get('/error', (req,res)=>{
+	res.render('errorPage.hbs');
+});
+
+
 app.post('/pushFor', (req, res) => {
 
-	getandIncrement(req, res, 1, true);
+	var userIP = getUserIP(req);
+	var pushStatus = "Not pushed";
+
+	checkLogsForPreviousUser(userIP).then((result) => {
+		if (result) {
+
+			getandIncrement(req, res, 1, true).then(() => {
+				logUserData(req, 1);
+			}).then(() => {
+				pushStatus = "success";
+			}).catch((err) => {
+				console.log("Push for Failed");
+				pushStatus = "Failed";
+			})
+
+
+		} else {
+
+			pushStatus = "Non-unique";
+			res.send(pushStatus);
+		}
+	})
 
 })
 
 app.post('/pushAgainst', (req, res) => {
 
-	getandIncrement(req, res, 1, false);
+	var userIP = getUserIP(req);
+	var pushStatus = "Not pushed";
+
+	checkLogsForPreviousUser(userIP).then((result) => {
+		if (result) {
+
+			getandIncrement(req, res, 1, false).then(() => {
+				logUserData(req, 0);
+			}).then(()=>{
+				pushStatus = "success";
+			}).catch((err) => {
+				console.log("Push for Failed");
+				pushStatus = "Failed";
+			});
+		
+		} else {
+
+			pushStatus = "Non-unique";
+			res.send(pushStatus);
+		}
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
@@ -79,9 +147,9 @@ app.get('/hello', (req, res) => {
 		against: 0
 	};
 
-	axios.put('https://btnproject-eef7a.firebaseio.com/counter.json', defaultVotes).then((resp) => {
-
-	})
+	axios.put('https://btnproject-eef7a.firebaseio.com/counter.json', defaultVotes).catch((err) => {
+		console.log("Error Putting /hello seed -" + err);
+	});
 
 })
 
@@ -89,54 +157,59 @@ app.get('/hello', (req, res) => {
 var getandIncrement = (req, res, data, vote) => {
 	//gets increments by argument and returns value. 
 	//first argument is the response object, that has method .send to send to response.
-	axios.get('https://btnproject-eef7a.firebaseio.com/counter.json').then((resp) => {
+	return new Promise((resolve, reject) => {
+		axios.get('https://btnproject-eef7a.firebaseio.com/counter.json').then((resp) => {
 
-		var latestValue = resp.data.value;
-		latestValue = latestValue + data;
-		latestDate = resp.data.date;
-		latestIP = resp.data.loc;
-		currentFor = resp.data.for || 0;
-		currentAgainst = resp.data.against || 0;
+			var latestValue = resp.data.value;
+			latestValue = latestValue + data;
+			latestDate = resp.data.date;
+			latestIP = resp.data.loc;
+			currentFor = resp.data.for || 0;
+			currentAgainst = resp.data.against || 0;
 
-		var ipGuest = req.headers['x-forwarded-for'];
+			//var ipGuest = req.headers['x-forwarded-for'];
+			var ipGuest = getUserIP(req);
 
-
-
-
-		var geo = getloc(null, latestIP, "city");
-
-
-		if (vote == true) {
-
-			axios.put('https://btnproject-eef7a.firebaseio.com/counter.json', { value: latestValue, date: Date.now(), loc: ipGuest, for: currentFor + 1, against: currentAgainst }).then((resp) => {
-
-			}).then(() => {
-				res.send({
-					value: latestValue,
-					date: latestDate,
-					currentTime: Date.now(),
-					loc: geo,
-					for: currentFor + 1,
-					against: currentAgainst
-				});
-			})
-		} else {
-			axios.put('https://btnproject-eef7a.firebaseio.com/counter.json', { value: latestValue, date: Date.now(), loc: ipGuest, for: currentFor, against: currentAgainst + 1 }).then((resp) => {
-
-			}).then(() => {
-				res.send({
-					value: latestValue,
-					date: latestDate,
-					currentTime: Date.now(),
-					loc: geo,
-					for: currentFor,
-					against: currentAgainst + 1
-				});
-			})
-		}
+			var geo = getloc(null, latestIP, "city");
 
 
-	});
+			if (vote == true) {
+
+				axios.put('https://btnproject-eef7a.firebaseio.com/counter.json', { value: latestValue, date: Date.now(), loc: ipGuest, for: currentFor + 1, against: currentAgainst }).then(() => {
+					res.send({
+						value: latestValue,
+						date: latestDate,
+						currentTime: Date.now(),
+						loc: geo,
+						for: currentFor + 1,
+						against: currentAgainst
+					});
+				}).catch((err) => {
+					console.log("Error Sending getandIncrement YesVote data -" + err);
+					reject();
+				})
+			} else {
+				axios.put('https://btnproject-eef7a.firebaseio.com/counter.json', { value: latestValue, date: Date.now(), loc: ipGuest, for: currentFor, against: currentAgainst + 1 }).then(() => {
+					res.send({
+						value: latestValue,
+						date: latestDate,
+						currentTime: Date.now(),
+						loc: geo,
+						for: currentFor,
+						against: currentAgainst + 1
+					});
+				}).catch((err) => {
+					console.log("Error Sending getandIncrement NoVote data -" + err);
+					reject();
+				})
+			}
+		}).then((data) => {
+			resolve();
+		}).catch((err) => {
+			console.log("Error Getting getandIncrement data -" + err);
+			reject();
+		});
+	})
 }
 
 
@@ -144,7 +217,7 @@ var getandIncrement = (req, res, data, vote) => {
 function getloc(req, ip, property) {
 
 	if (req) {
-		var ipGuest = req.headers['x-forwarded-for'];
+		var ipGuest = getUserIP(req);
 	} else {
 		var ipGuest = ip;
 	}
@@ -158,7 +231,7 @@ function getloc(req, ip, property) {
 
 	if (geo[property] === null) {
 		geo = "an Unknown Location"
-	
+
 		return geo;
 	}
 	return geo[property];
@@ -172,6 +245,85 @@ function getloc(req, ip, property) {
 		ll: [ 29.4889, -98.3987 ],
 	metro: 641,
 	zip: 78218 }*/
+
+}
+
+
+function logUserData(req, vote) {
+	// Take current request headers and create table entry:
+	// user ip, time, vote.
+	var userIP = getUserIP(req);
+
+
+	if (!userIP) {
+		//if userIP cannot be resolved it generates a unique code for the user.
+		//this allows the multiple Unidentified IPs to continue voting.
+		userIP = "Unknown" + uid();
+	}
+
+	var data = { userIP: userIP, vote: vote }
+	axios.post('https://btnproject-eef7a.firebaseio.com/logs.json', data).then((resp) => {
+		console.log(`Data logged: User: ${userIP}, Vote: ${vote}`);
+	}).catch((err) => {
+		console.log("Error posting in log user data -" + err);
+	});
+}
+
+function getLogs() {
+
+	return new Promise((resolve, reject) => {
+		var logsObject;
+		//retrieve logs array fromd atabase:
+		axios.get('https://btnproject-eef7a.firebaseio.com/logs.json').then((data) => {
+			logsObject = data;
+			resolve(data);
+		}).catch((error) => {
+			console.log("error retrieving logs - " + error);
+			reject();
+		})
+	})
+}
+
+function checkLogsForPreviousUser(userIP) {
+	//get logs then check the logs file if the specified IP already exists.
+	//If so return false, else return true.
+	return new Promise((resolve, reject) => {
+		var unique = true;
+		getLogs().then((data) => {
+			if (data.data) {
+				jsonForEach(data, (elem) => {
+					if (elem.userIP == userIP) {
+						console.log(elem.userIP + " matches with " + userIP + "Insert failed");
+						unique = false;
+					}
+
+				})
+			}
+		}).catch((error) => {
+			console.log("getLogs failed! - " + error);
+			reject();
+		})
+		resolve(unique);
+	})
+
+
+}
+
+function getUserIP(req) {
+	// retrieve user IP from req object
+	// Build this function to be more accurate/use more sources.
+	var ip = req.headers['x-forwarded-for'];
+	return ip;
+}
+
+function jsonForEach(object, callback) {
+	//takes in a JSON object, iterates through it.
+
+	Object.keys(object.data).forEach((element) => {
+		callback(object.data[element]);
+	})
+
+
 
 }
 
--- public/clickLogic.js
+++ public/clickLogic.js
@@ -1,60 +1,67 @@
-import {serverPOST,serverGET} from './xhttpFunctions.js';
+import { serverPOST, serverGET } from './xhttpFunctions.js';
 
-function winload(){
+function winload() {
 
-	var forButton = document.getElementById("forButton").addEventListener("click", ()=>{
+	var forButton = document.getElementById("forButton").addEventListener("click", () => {
 
 		console.log("forButtonClicked");
 
-		
-		serverPOST('/pushFor',(res)=>{
-			var serverResponse = JSON.parse(res);
-			console.log("vasl"+serverResponse);
-			var currentCount = serverResponse.value;
-			var dateObject = (formatDateSince(serverResponse.currentTime,serverResponse.date));
-			document.getElementById("counter").innerHTML="Votes: "+currentCount;
-			document.getElementById("counterSubtext").innerHTML=`Last vote was ${dateObject.value} ${dateObject.name} ago from ${serverResponse.loc}.`;
-			document.getElementById("currentFor").innerHTML=`For: ${serverResponse.for}`;
-			document.getElementById("currentAgainst").innerHTML=`Against: ${serverResponse.against}`;
 
-		},'')
+		serverPOST('/pushFor', (res) => {
+			if (serverResponseCheck(res)) {
+				var serverResponse = JSON.parse(res);
+
+				var currentCount = serverResponse.value;
+				var dateObject = (formatDateSince(serverResponse.currentTime, serverResponse.date));
+				document.getElementById("counter").innerHTML = "Votes: " + currentCount;
+				document.getElementById("counterSubtext").innerHTML = `Last vote was ${dateObject.value} ${dateObject.name} ago from ${serverResponse.loc}.`;
+				document.getElementById("currentFor").innerHTML = `For: ${serverResponse.for}`;
+				document.getElementById("currentAgainst").innerHTML = `Against: ${serverResponse.against}`;
+			}else{
+
+			}
+
+		}, '')
 	})
 
-	var againstButton = document.getElementById("againstButton").addEventListener("click", ()=>{
+	var againstButton = document.getElementById("againstButton").addEventListener("click", () => {
 
 		console.log("AgainstButtonClicked");
-		
-		serverPOST('/pushAgainst',(res)=>{
-			var serverResponse = JSON.parse(res);
 
-			console.log("vasl"+serverResponse);
-			var currentCount = serverResponse.value;
-			var dateObject = (formatDateSince(serverResponse.currentTime,serverResponse.date));
-			document.getElementById("counter").innerHTML="Votes: "+currentCount;
-			document.getElementById("counterSubtext").innerHTML=`Last vote was ${dateObject.value} ${dateObject.name} ago from ${serverResponse.loc}.`;
-			document.getElementById("currentFor").innerHTML=`For: ${serverResponse.for}`;
-			document.getElementById("currentAgainst").innerHTML=`Against: ${serverResponse.against}`;
 
-		},'')
+		serverPOST('/pushAgainst', (res) => {
+			if (serverResponseCheck(res)) {
+				var serverResponse = JSON.parse(res);
+
+				var currentCount = serverResponse.value;
+				var dateObject = (formatDateSince(serverResponse.currentTime, serverResponse.date));
+				document.getElementById("counter").innerHTML = "Votes: " + currentCount;
+				document.getElementById("counterSubtext").innerHTML = `Last vote was ${dateObject.value} ${dateObject.name} ago from ${serverResponse.loc}.`;
+				document.getElementById("currentFor").innerHTML = `For: ${serverResponse.for}`;
+				document.getElementById("currentAgainst").innerHTML = `Against: ${serverResponse.against}`;
+			}else{
+				
+			}
+		}, '')
 	})
 
 
 
 
-	var showButton = document.getElementById("showResults").addEventListener("click", ()=>{
+	var showButton = document.getElementById("showResults").addEventListener("click", () => {
 
-		if(document.getElementById("currentResults").style.visibility=='hidden'){
+		if (document.getElementById("currentResults").style.visibility == 'hidden') {
 
-			document.getElementById("currentResults").style.visibility='visible';
+			document.getElementById("currentResults").style.visibility = 'visible';
 			document.getElementById("showResults").innerHTML = "Hide Results";
-		}else{
+		} else {
 
-			document.getElementById("currentResults").style.visibility='hidden';
+			document.getElementById("currentResults").style.visibility = 'hidden';
 			document.getElementById("showResults").innerHTML = "Show Results"
 		}
 
 		document.getElementById("showResults").blur();
-	
+
 	});
 
 
@@ -62,36 +69,45 @@ function winload(){
 
 window.onload = winload;
 
-function formatDateSince(currentServerTime,dateDifference){
+function formatDateSince(currentServerTime, dateDifference) {
 	//takes in dateNow value (miliseconds) and returns
 	//either seconds,minutes,hours,days etc if that value is > 2.
 
-	function convertSeconds(dateValue){
-		return dateValue/1000;
+	function convertSeconds(dateValue) {
+		return dateValue / 1000;
 	}
 
-	var seconds = (convertSeconds(Date.now())-dateDifference/1000)
-	var minutes = seconds/60;
-	var hours = minutes/60;
-	var days = hours/60;
+	var seconds = (convertSeconds(Date.now()) - dateDifference / 1000)
+	var minutes = seconds / 60;
+	var hours = minutes / 60;
+	var days = hours / 60;
 
 	var ElementArray = [
-	{name:"Seconds",value:seconds},
-	{name:"Minutes",value:minutes},
-	{name:"Hours",value:hours},
-	{name:"Days",value:days}
+		{ name: "Seconds", value: seconds },
+		{ name: "Minutes", value: minutes },
+		{ name: "Hours", value: hours },
+		{ name: "Days", value: days }
 	];
 
-	var dateValue = ElementArray[0].value; 
+	var dateValue = ElementArray[0].value;
 	var formattedDate = ElementArray[0];
 
-	for(var x = 1; x < ElementArray.length;x++){
+	for (var x = 1; x < ElementArray.length; x++) {
 
-		if((ElementArray[x].value < dateValue)&&(ElementArray[x].value>2)){
-			formattedDate = ElementArray[x];		
+		if ((ElementArray[x].value < dateValue) && (ElementArray[x].value > 2)) {
+			formattedDate = ElementArray[x];
 		}
 	}
 
 	formattedDate.value = Math.floor(formattedDate.value);
 	return formattedDate;
+}
+
+function serverResponseCheck(response) {
+	// Checks server response to make sure not faulty error code.
+	if (response == "Not pushed" || "Failed" || "Non-unique") {
+		return false;
+	} else {
+		return true;
+	}
 }
